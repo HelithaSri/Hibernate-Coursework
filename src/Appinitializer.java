087e92557680c29b469e339cea5f9ce8a325782d
HelithaSri <helitha.pravin@gmail.com>
2021-12-22 17:33:37 +0530

    Add Appinitializer

---
+++ src/Appinitializer.java
@@ -0,0 +1,20 @@
+/**
+ * @author HelithaSri
+ * @created 12/22/2021 - 5:25 PM
+ * @project HibernateCW
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Appinitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
