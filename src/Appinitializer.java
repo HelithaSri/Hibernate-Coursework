a32d4094819409253d93346e99bf4a29eea9ab47
HelithaSri <helitha.pravin@gmail.com>
2021-12-23 15:18:11 +0530

    Update Name

--- src/Appinitializer.java
+++ src/Appinitializer.java
@@ -19,7 +19,7 @@ public class Appinitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/StudentRegistration.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/Base.fxml"))));
         primaryStage.show();
     }
 }
---
+++ src/controller/BaseController.java
@@ -0,0 +1,9 @@
+package controller;
+
+/**
+ * @author HelithaSri
+ * @created 12/23/2021 - 3:17 PM
+ * @project HibernateCW
+ */
+public class BaseController {
+}
