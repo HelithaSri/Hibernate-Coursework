ac6ac1fa77e1cc61de659820ae233c59802f090c
HelithaSri <helitha.pravin@gmail.com>
2021-12-23 18:49:59 +0530

    Student Register UI

--- src/controller/BaseController.java
+++ src/controller/BaseController.java
@@ -1,9 +1,23 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 /**
  * @author HelithaSri
  * @created 12/23/2021 - 3:17 PM
  * @project HibernateCW
  */
 public class BaseController {
+    public AnchorPane rightMainAnchor;
+
+    public void btnRegisterStudent(MouseEvent mouseEvent) throws IOException {
+        AnchorPane pane;
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("../view/RegisterStudent.fxml"));
+        pane = fxmlLoader.load();
+        rightMainAnchor.getChildren().setAll(pane);
+    }
 }
---
+++ src/controller/RegisterStudentController.java
@@ -0,0 +1,10 @@
+package controller;
+
+/**
+ * @author HelithaSri
+ * @created 12/23/2021 - 3:19 PM
+ * @project HibernateCW
+ */
+public class RegisterStudentController {
+
+}
