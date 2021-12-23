69b921543a322fbfa773dd2fb13e6276ac1b4181
HelithaSri <helitha.pravin@gmail.com>
2021-12-23 14:47:42 +0530

    Change Resolution

--- src/Appinitializer.java
+++ src/Appinitializer.java
@@ -19,7 +19,7 @@ public class Appinitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/LogingForm.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/StudentRegistration.fxml"))));
         primaryStage.show();
     }
 }
